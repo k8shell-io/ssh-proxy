c5f52cd152431c34d268a3deda1062bea6bfc816
bruckins <karelnebojsa@seznam.cz>
2025-09-24 15:36:11 +0200

    prov error clean up

--- internal/server/state.go
+++ internal/server/state.go
@@ -254,7 +254,7 @@ func (c *ConnectionInfo) CreateK8shelldClient(ctx context.Context, writer io.Wri
 	if err != nil {
 		var provisionErr *workspace.ProvisionError
 		if errors.As(err, &provisionErr) {
-			infoWriter.WriteError("Provisioning error: " + provisionErr.Message)
+			infoWriter.WriteError(provisionErr.Message)
 		}
 		return nil, fmt.Errorf("failed to ensure workspace for user %s: %w", c.userStr.Username, err)
 	}
