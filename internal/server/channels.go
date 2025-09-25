581638cf72f6ebe8d2eccadb7c492ea0b18c2dc6
bruckins <karelnebojsa@seznam.cz>
2025-09-25 19:05:40 +0200

    graceful shutdown fix

--- internal/server/channels.go
+++ internal/server/channels.go
@@ -7,29 +7,46 @@ import (
 
 // handleChannels handles SSH channel requests.
 func (s *Server) handleChannels(sshConn *ssh.ServerConn, connInfo *ConnectionInfo, channels <-chan ssh.NewChannel) {
-	for newChannel := range channels {
-		s.log.Debug().Msgf("Received channel request: type=%s", newChannel.ChannelType())
-		switch newChannel.ChannelType() {
-		case "session":
-			go s.handleSessionChannel(sshConn, connInfo, newChannel)
-		case "direct-tcpip":
-			connInfo.AddChannelInfo(models.ChannelShortPf)
-			go s.handleDirectTCPIPChannel(sshConn, connInfo, newChannel)
-		default:
-			s.log.Warn().Msgf("Unsupported channel type: %s", newChannel.ChannelType())
-			newChannel.Reject(ssh.UnknownChannelType, "channel type not supported")
+	for {
+		select {
+		case <-s.ctx.Done():
+			s.log.Info().Msg("Channel handler stopping due to context cancellation")
+			return
+		case channel := <-channels:
+			if channel == nil {
+				return
+			}
+			s.log.Debug().Msgf("Received channel request: type=%s", channel.ChannelType())
+			switch channel.ChannelType() {
+			case "session":
+				go s.handleSessionChannel(sshConn, connInfo, channel)
+			case "direct-tcpip":
+				connInfo.AddChannelInfo(models.ChannelShortPf)
+				go s.handleDirectTCPIPChannel(sshConn, connInfo, channel)
+			default:
+				s.log.Warn().Msgf("Unsupported channel type: %s", channel.ChannelType())
+				channel.Reject(ssh.UnknownChannelType, "channel type not supported")
+			}
 		}
 	}
 }
 
 // handleGlobalRequests processes SSH global requests
 func (s *Server) handleGlobalRequests(requests <-chan *ssh.Request) {
-	for req := range requests {
-		s.log.Debug().Msgf("Received global request: type=%s, want_reply=%t", req.Type, req.WantReply)
+	for {
+		select {
+		case <-s.ctx.Done():
+			s.log.Info().Msg("Global request handler stopping due to context cancellation")
+			return
+		case req := <-requests:
+			if req == nil {
+				return
+			}
+			s.log.Debug().Msgf("Received global request: type=%s, want_reply=%t", req.Type, req.WantReply)
 
-		// Reject unknown global requests
-		if req.WantReply {
-			req.Reply(false, nil)
+			if req.WantReply {
+				req.Reply(false, nil)
+			}
 		}
 	}
 }
