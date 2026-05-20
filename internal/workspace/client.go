62c0bdf725151f8d463511cf3df494329523936f
n/a <karelnebojsa@seznam.cz>
2026-05-20 18:58:20 +0200

    init scripts status flag

--- internal/workspace/client.go
+++ internal/workspace/client.go
@@ -21,7 +21,7 @@ import (
 type K8shelldClient interface {
 	Handshake(ctx context.Context, userToken string) (*k8shelldv1.HandshakeResponse, error)
 	RunShell(ctx context.Context, userToken string, asUser string, upstream k8shelldClient.BufferedReadWriter, sessionId string, envVars []string,
-		width, height uint32, usePty bool, lockId string, detachOnClose bool, enableRecording bool,
+		width, height uint32, usePty bool, lockId string, detachOnClose bool, showInitScriptsStatus bool, enableRecording bool,
 		notifyPtyName k8shelld.NotifyPtyNameFunc) error
 	ResizeTerminal(ctx context.Context, sessionId string, width, height uint32) error
 	RunUnixSocket(ctx context.Context, userToken string, upstream k8shelldClient.BufferedReadWriter, unixSocketId, socketPath, mode string) error
