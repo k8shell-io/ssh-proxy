bccc29704fee03e08e50a69406903b83478817af
n/a <karelnebojsa@seznam.cz>
2026-06-25 11:46:25 +0200

    comments

--- internal/server/directstream.go
+++ internal/server/directstream.go
@@ -1,3 +1,7 @@
+// Copyright 2025 The K8shell Authors. All rights reserved.
+// Use of this source code is governed by a AGPLv3
+// license that can be found in the LICENSE file.
+
 package server
 
 import (
