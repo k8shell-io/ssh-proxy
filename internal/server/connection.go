bdfa115eaf39e7a666aee640d1982bfebc5079dc
n/a <karelnebojsa@seznam.cz>
2026-07-02 23:12:45 +0200

    token fix

--- internal/server/connection.go
+++ internal/server/connection.go
@@ -469,7 +469,13 @@ func (c *Connection) Handshake(writer io.Writer, writerOptions *workspace.InfoWr
 	c.log.Debug().Msgf("Connecting to k8shelld at %s:%d for user %s, version: %s",
 		status.ServerName, status.Port, c.user.Username, status.AppVersion)
 
-	handshake, err := k8shelld.Handshake(c.ctx, "")
+	userToken, err := c.GetUserToken()
+	if err != nil {
+		infoWriter.WriteSystemError("Failed to obtain access token.")
+		return nil, fmt.Errorf("failed to get user token for user %s: %w", c.user.Username, err)
+	}
+
+	handshake, err := k8shelld.Handshake(c.ctx, userToken)
 	if err != nil {
 		msg := grpcClientMessage(err)
 		if s, ok := grpcstatus.FromError(err); ok && s.Code() == grpccodes.Unavailable {
